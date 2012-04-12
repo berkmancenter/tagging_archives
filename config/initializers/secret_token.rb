b7a52a9ffc3ce09fbc3bd37c98499b97dec2e450
Anita <anita@anitapatel.com>
2012-04-12 10:23:38 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TaggingArchives::Application.config.secret_token = '49a8f243920c09dcb3c35d0db973fa95cb1760c3109626472b97cf6e3af8409e67329c3ba9bb803e74025589d5f0359ffd71cfb9724bf72bfe2f1c64924b9002'
