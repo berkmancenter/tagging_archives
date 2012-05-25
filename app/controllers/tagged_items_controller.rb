ba09c088edb1ac9ee99ead917c68369684b09f4a
Anita <anita@anitapatel.com>
2012-05-25 15:54:53 -0400

    searching

--- app/controllers/tagged_items_controller.rb
+++ app/controllers/tagged_items_controller.rb
@@ -83,56 +83,15 @@ class TaggedItemsController < ApplicationController
   end
   
   def item_search
+    Post.search do
+      fulltext 'best pizza'
 
-    if params[:junction_type].blank?
-      params[:junction_type] = 'and'
+      with :blog_id, 1
+      with(:published_at).less_than Time.now
+      order_by :published_at, :desc
+      paginate :page => 2, :per_page => 15
+      facet :category_ids, :author_id
     end
-    # no code injection here.
-    junction_type_map = {'and' => 'all_of', 'or' => 'any_of'}
-
-    unless params[:include_tag_ids].blank?
-      include_tags = params[:include_tag_ids]
-    end
-
-    unless params[:q].blank?
-      keywords = params[:q]
-    end
-
-    if ! include_tags.blank? || ! keywords.blank?
-      @search = TaggedItem.search
-      hub_id = @tagged_item.id
-
-      hub_context = @tagged_item.tagging_key
-
-      @search.build do
-        self.send((junction_type_map[params[:junction_type]].blank?) ? 'any_of' : junction_type_map[params[:junction_type]]) do
-          unless include_tags.blank?
-            with :tag_contexts, include_tags.collect{|it| %Q|#{hub_context}-#{it}|}
-          end
-          unless params[:q].blank?
-            text_fields do
-              any_of do
-                with :title, params[:q]
-                with :urn, params[:q]
-                with :tag_list, params[:q]
-              end
-            end
-          end
-        end
-      end
-
-      @search.build do
-        with :hub_ids, hub_id
-        paginate :page => params[:page], :per_page => get_per_page
-        order_by(:date_published, :desc)
-      end
-
-      @search.execute!
-    end
-    respond_to do|format|
-      format.html{ render :layout => ! request.xhr? }
-      format.json{ render_for_api :default, :json => (@search.blank?) ? [] : @search.results }
-      format.xml{ render_for_api :default, :xml => (@search.blank?) ? [] : @search.results }
-    end
+    
   end
 end
--- app/models/tagged_item.rb
+++ app/models/tagged_item.rb
@@ -1,4 +1,14 @@
 class TaggedItem < ActiveRecord::Base
   acts_as_taggable
 
+  searchable do
+    text :title, :more_like_this => true
+    text :urn, :more_like_this => true
+    text :tag_list, :more_like_this => true
+
+    string :tag_list, :multiple => true
+
+    string :title
+    string :urn
+  end
 end
