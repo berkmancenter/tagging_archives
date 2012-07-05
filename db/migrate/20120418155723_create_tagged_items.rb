bcd262dc1fc5ba26d9f3cb164457e51ea2624b90
Anita <anita@anitapatel.com>
2012-07-05 11:00:56 -0400

    fixed tagged items migration

--- db/migrate/20120418155723_create_tagged_items.rb
+++ db/migrate/20120418155723_create_tagged_items.rb
@@ -3,13 +3,11 @@ class CreateTaggedItems < ActiveRecord::Migration
     create_table :tagged_items do |t|
       t.string :title
       t.string :urn
-      t.string :finding_aid
       t.text :notes
       t.timestamps
     end
     
     add_index :tagged_items, :title
     add_index :tagged_items, :urn
-    add_index :tagged_items, :finding_aid
   end
 end
